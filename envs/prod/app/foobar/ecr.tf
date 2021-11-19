bb1c427487e0980b239e4a85912f6ae8e22dc55c
Kento Nakamura <kento.nakamura@sencorp.co.jp>
2021-11-20 00:30:30 +0900

    ecrのlifecyclepolicyの設定

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -4,3 +4,23 @@ resource "aws_ecr_repository" "nginx" {
     Name = "example-prod-foobar-nginx"
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "nginx" {
+  policy = jsonencode(
+    {
+      "rules" : [
+        {
+          "rulePriority" : 1,
+          "description" : "Hold only 10 images, expire all others",
+          "selection" : {
+            "tagStatus" : "any",
+            "countType" : "imageCountMoreThan",
+            "countNumber" : 10
+          },
+          "action" : {
+            "type" : "expire"
+          }
+      }]
+  })
+  repository = aws_ecr_repository.nginx.name
+}
