cb1df6c3eff651081070217cf65521f27ba7ff07
Kento Nakamura <kento.nakamura@sencorp.co.jp>
2021-11-20 16:41:40 +0900

    ecr moduleの使用

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -1,26 +1,5 @@
-resource "aws_ecr_repository" "nginx" {
-  name = "example-prod-foobar-nginx"
-  tags = {
-    Name = "example-prod-foobar-nginx"
-  }
-}
+module "nginx" {
+  source = "../../../../modules/ecr"
 
-resource "aws_ecr_lifecycle_policy" "nginx" {
-  policy = jsonencode(
-    {
-      "rules" : [
-        {
-          "rulePriority" : 1,
-          "description" : "Hold only 10 images, expire all others",
-          "selection" : {
-            "tagStatus" : "any",
-            "countType" : "imageCountMoreThan",
-            "countNumber" : 10
-          },
-          "action" : {
-            "type" : "expire"
-          }
-      }]
-  })
-  repository = aws_ecr_repository.nginx.name
+  name = "example-prod-foobar-nginx"
 }
